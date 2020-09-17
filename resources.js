aa415d2ae5a30b49f4d05318196cd42847286a34
Donna Engelberger <dengelberger@gmail.com>
2020-09-17 10:57:10 -0400

    last night's changes

--- resources.js
+++ resources.js
@@ -2,10 +2,26 @@
 
 //AN ARRAY OF TERMS AND DEFINITIONS AND MAYBE IMAGES TO DRAW FROM?
 
-
+definitionsAndTerms = [
+    {   "Dow_Jones_Industrial_Average" : "",
+        "Nasdaq_Composite" : "",
+        "Stock_Price" : "",
+        "S&P500" : "",
+        "Five_Day_Trend" : "",
+        "Graph" : "",
+        "IPO" : "",
+        "Market_level_indicator" : "",
+        "Sector-specific_indicator" : "",
+    }
+]
 
 //ON-CLICK EVENT LISTENERS FOR THE TERMS ON THE LEFT SIDE MENU
 
+$(document).on('click', '.termsMenu', function () {
 
+    // var savedCity = $(this).text();
+    // searchCity(savedCity);
+
+});
 
 //ARE THE RESOURCES WE LIST GOING TO BE HREFS?
