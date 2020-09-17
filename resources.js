8e1c2f3068fc41337d08a5eea80bec1ad0e36524
Donna Engelberger <dengelberger@gmail.com>
2020-09-17 14:29:13 -0400

    as of 2:30 on Thursday

--- resources.js
+++ resources.js
@@ -1,27 +1,10 @@
 //THIS IS THE JAVASCRIPT FOR THE RESOURCES PAGE
 
-//AN ARRAY OF TERMS AND DEFINITIONS AND MAYBE IMAGES TO DRAW FROM?
+//function to accordion the terms list
 
-definitionsAndTerms = [
-    {   "Dow_Jones_Industrial_Average" : "",
-        "Nasdaq_Composite" : "",
-        "Stock_Price" : "",
-        "S&P500" : "",
-        "Five_Day_Trend" : "",
-        "Graph" : "",
-        "IPO" : "",
-        "Market_level_indicator" : "",
-        "Sector-specific_indicator" : "",
-    }
-]
-
-//ON-CLICK EVENT LISTENERS FOR THE TERMS ON THE LEFT SIDE MENU
-
-$(document).on('click', '.termsMenu', function () {
-
-    // var savedCity = $(this).text();
-    // searchCity(savedCity);
-
+$(function() {
+    $("#termsMenu").accordion();
 });
+
 
 //ARE THE RESOURCES WE LIST GOING TO BE HREFS?
