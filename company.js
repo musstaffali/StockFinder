13dda7312aa1dfd36fb56636b34df9ea352f8484
CarlosR317 <cramire5@ycp.edu>
2020-09-18 10:27:27 -0400

    js update

--- company.js
+++ company.js
@@ -3,29 +3,42 @@ $(document).ready(function () {
     $('#submitBtn').on('click', function (event) {
         event.preventDefault();
         var compInfo = $('#companySearch').val();
-            searchCompany(compInfo);
-          })
+        searchCompany(compInfo);
+    })
 
     function searchCompany(compInfo) {
         var findComp = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=" + compInfo + "&apikey=KMCDXB4JI059DXWG";
-        
+
 
         $.ajax({ url: findComp, method: 'GET' })
             .done(function (response) {
                 console.log(response);
 
-                var company = $("<h1>").text(response.Name);
+                var company = $("<h1>").text("Name: " + response.Name);
+                var assetType = $('<p>').text("Asset Type: " + response.AssetType);
+                var descript = $('<p>').text("Description: " + response.Description);
                 var display = $("<div>").addClass("card-body");
 
+                display.append(descript)
                 display.append(company)
+                display.append(assetType)
                 $('#companyList').append(display);
-              
-                    var incomeState = "https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=" + compInfo + "&apikey=KMCDXB4JI059DXWG";
-                    $.ajax({url: incomeState, method: 'GET'}).then(function(incomeInfo){
-                        console.log(incomeInfo);
-                    })
+
+                var incomeState = "https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=" + compInfo + "&apikey=KMCDXB4JI059DXWG";
+
+                $.ajax({ url: incomeState, method: 'GET' })
+                .then(function (compInfo) {
+                    console.log(compInfo);
+
+                    var income = $('<p>').text(compInfo.symbol);
+                                       
+                    display.append(income)
+                    $('#companyList').prepend(display);
+                })
             })
-             }
-         
-            
+    }
+
+
 })
+
+// + Symbol + AssetType + AnalystTargetPrice + Industry + GrossProfitTTM
