e66b44facd061054b592f6cf33ee56fbff3f7659
CarlosR317 <cramire5@ycp.edu>
2020-09-18 18:17:05 -0400

    changes

--- company.js
+++ company.js
@@ -29,9 +29,9 @@ $(document).ready(function () {
                 success: function (results) {
                     console.log(results);
                     searchResults = results;
-                
+
                     localStorage.saveData = JSON.stringify(results);
-                   
+
 
                     var query = "https://cloud.iexapis.com/stable/stock/" + symbol + "/news?token=pk_671c931364a84a08aae2391ce68605f7"
                     $.ajax({ url: query, method: 'GET' })
@@ -42,32 +42,64 @@ $(document).ready(function () {
                             console.log(searchResults["Global Quote"]);
 
                             var industry = $("<a>").text("Headline/Link: " + news[0].headline);
+                            var industryone = $("<a>").text("Headline/Link: " + news[1].headline);
+                            var industrytwo = $("<a>").text("Headline/Link: " + news[2].headline);
+                            var industrythr = $("<a>").text("Headline/Link: " + news[3].headline);
+                            var industryfou = $("<a>").text("Headline/Link: " + news[4].headline);
                             var summary = $("<p>").text("Summary: " + news[0].summary);
+                            var summaryone = $("<p>").text("Summary: " + news[1].summary);
+                            var summarytwo = $("<p>").text("Summary: " + news[2].summary);
+                            var summarythr = $("<p>").text("Summary: " + news[3].summary);
+                            var summaryfou = $("<p>").text("Summary: " + news[4].summary);
                             industry.attr("src", news[0].url);
                             industry.click(function () {
                                 location.href = news[0].url
                             });
-                            var sym = $("<p>").text("Symbol: " + searchResults["Global Quote"]["01. symbol"] || searchResults["Global Quote"]["1. symbol"]);
-                            var high = $("<p>").text("High: " + searchResults["Global Quote"]["03. high"]);
-                            var low = $("<p>").text("Low: " + searchResults["Global Quote"]["04. low"]);
-                            var price = $("<p>").text("Price: " + searchResults["Global Quote"]["05. price"]);
+                            industryone.attr("src", news[1].headline);
+                            industryone.click(function () {
+                                location.href = news[1].url
+                            });
+                            industrytwo.attr("src", news[2].url);
+                            industrytwo.click(function () {
+                                location.href = news[2].url
+                            });
+                            industrythr.attr("src", news[3].url);
+                            industrythr.click(function () {
+                                location.href = news[3].url
+                            });
+                            industryfou.attr("src", news[4].url);
+                            industryfou.click(function () {
+                                location.href = news[4].url
+                            });
+                                var sym = $("<p>").text("Symbol: " + searchResults["Global Quote"]["01. symbol"] || searchResults["Global Quote"]["1. symbol"]);
+                                var high = $("<p>").text("High: " + searchResults["Global Quote"]["03. high"]);
+                                var low = $("<p>").text("Low: " + searchResults["Global Quote"]["04. low"]);
+                                var price = $("<p>").text("Price: " + searchResults["Global Quote"]["05. price"]);
 
-                            var display = $("<div>").addClass("card-body");
+                                var display = $("<div>").addClass("card-body");
 
 
-                            display.append(sym);
-                            display.append(high);
-                            display.append(low);
-                            display.append(price);
-                            display.append(industry);
-                            display.append(summary);
+                                display.append(sym);
+                                display.append(high);
+                                display.append(low);
+                                display.append(price);
+                                display.append(industry);
+                                display.append(summary);
+                                display.append(industryone);
+                                display.append(summaryone);
+                                display.append(industrytwo);
+                                display.append(summarytwo);
+                                display.append(industrythr);
+                                display.append(summarythr);
+                                display.append(industryfou);
+                                display.append(summaryfou);
 
 
-                            $('#companyList').prepend(display);
+                                $('#companyList').prepend(display);
 
 
-                        })
-                }
+                            })
+                        }
             });
         }
         )
